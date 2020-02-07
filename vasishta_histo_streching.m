a89682f3c90d51c9696b42420fe53f79929f5cb1
vasishtapatel <55794905+vasishtapatel@users.noreply.github.com>
2020-02-07 12:37:52 +0530

    Add files via upload

---
+++ vasishta_histo_streching.m
@@ -0,0 +1,12 @@
+clc,
+clear;
+close all;
+image = imread ('C:\Users\vasishta patel\Pictures\Saved Pictures\ei.jpg');
+
+stretched_Image = imadjust(image, stretchlim(image, [0.05, 0.95]),[]);
+
+subplot(2,2,1), imshow(image), title('Original Image');
+subplot(2,2,2), imshow(stretched_Image), title('Strethced Image');
+subplot(2,2,3), imhist(image), title('Histogram of Original Image');
+subplot(2,2,4), imhist(stretched_Image), title('Histogram of Stretched Image');
+
